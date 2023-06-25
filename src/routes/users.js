7ddbfc1320d35542e525a49812fc74ef3b5678ea
Alanquintana0 <65621524+Alanquintana0@users.noreply.github.com>
2023-06-24 19:39:10 -0600

    Se anadio la comprobacion que valida que no haya userNames repetidos, ademas de que se agregaron los paquetes para implementar JWT

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,39 +1,56 @@
-const express = require('express');
-const bcrypt = require('bcrypt');
-const userSchema = require('../models/user');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const userSchema = require("../models/user");
 
 const router = express.Router();
 
-router.post('/users', async (req, res) => {
-    var newUserName = req.body.userName;
-    var password = req.body.password;
-    var newName = req.body.name;
-    var newPhoneNumber = req.body.phoneNumber;
-    var newAddress = req.body.address;
+router.post("/users", async (req, res) => {
+  var newUserName = req.body.userName;
+  var password = req.body.password;
+  var newName = req.body.name;
+  var newPhoneNumber = req.body.phoneNumber;
+  var newAddress = req.body.address;
 
-    const salt = await bcrypt.genSalt(10);
-
-    const passwordHash = await bcrypt.hash(password, salt);
-
-    const User = {
-        userName: newUserName,
-        password: passwordHash,
-        name: newName,
-        phoneNumber: newPhoneNumber,
-        address: newAddress,
-        salt: salt
-    };
-
-    const newUser = userSchema(User);
-    
-    newUser.save()
-    .then((data) => {
-        res.json(data);
-    }).catch((err) => {
-        res.json({
-            message: err
-        })
+  const existingUser = await userSchema.findOne({ userName: newUserName });
+  if (existingUser) {
+    return res.status(400).json({
+      message: "Username already exists"
     });
-})
+  }
 
+  const salt = await bcrypt.genSalt(10);
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const User = {
+    userName: newUserName,
+    password: passwordHash,
+    name: newName,
+    phoneNumber: newPhoneNumber,
+    address: newAddress,
+    salt: salt,
+  };
+
+  const newUser = userSchema(User);
+
+  newUser
+    .save()
+    .then((data) => {
+      res.status(200).json({
+        message: "User created",
+        obj: data
+      })
+    })
+    .catch((err) => {
+      res.status(500).json({
+        message: "User not created",
+        error: err
+      });
+    });
+});
+
+/*
+router.post('/login', async(req, res) => {
+
+})
+*/
 module.exports = router;
